f677246698a3bf82f1eb610de7fed257e472127d
Yusei Mizutani <yusei4520030401@gmail.com>
2023-10-15 11:16:58 +0900

    SSR

---
+++ src/blogAPI.ts
@@ -0,0 +1,12 @@
+import { Article } from './types';
+
+export const getAllArticles = async (): Promise<Article[]> => {
+  
+  const res = await fetch('https://localhost:3001/posts', {
+    cache: 'no-store', // SSR 
+    // next: {revalidate: 10} // ISR 定期的にサーバーからデータを取得する
+    // cache: 'no-store' // SSG ビルド時、一度だけデータ取得
+  });
+  const articles = await res.json();
+  return articles;
+};
