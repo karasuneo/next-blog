6dae35078208a84b9549ec51c8fffad23df92d7e
Yusei Mizutani <yusei4520030401@gmail.com>
2023-10-16 14:24:51 +0900

    削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   // const articles = await getAllArticles();
 
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
+  
   const res = await fetch(`${API_URL}/api`, { cache: 'no-store' });
   const articles = await res.json();
 
