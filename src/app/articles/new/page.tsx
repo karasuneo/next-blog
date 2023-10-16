a81ecb0a1208abfd40af489eb7548207815f547c
Yusei Mizutani <yusei4520030401@gmail.com>
2023-10-16 09:54:05 +0900

    ローディング

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -4,18 +4,21 @@ import React, { useState } from 'react';
 
 import { createArticle } from '@/blogAPI';
 
-
 const CreateBlogPage = () => {
   const router = useRouter();
   const [id, setId] = useState<string>('');
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    setLoading(true);
+
     await createArticle(id, title, content);
 
+    setLoading(false);
     router.push(`/`);
     router.refresh();
   };
@@ -52,7 +55,12 @@ const CreateBlogPage = () => {
         </div>
         <button
           type="submit"
-          className="py-2 px-4 border rounded-md bg-orange-300"
+          className={`py-2 px-4 border rounded-md ${
+            loading
+              ? 'bg-orange-300 cursor-not-allowed'
+              : 'bg-orange-400 hover:bg-orange-500'
+          } `}
+          disabled={loading}
         >
           投稿
         </button>
